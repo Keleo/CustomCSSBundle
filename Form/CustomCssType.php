4cb3942a785f4c5350188f614e978189fe6fa837
Kevin Papst <kpapst@gmx.net>
2024-11-21 11:03:46 +0100

    phpstan 2

--- Form/CustomCssType.php
+++ Form/CustomCssType.php
@@ -17,6 +17,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * @extends AbstractType<CustomCss>
+ */
 class CustomCssType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
